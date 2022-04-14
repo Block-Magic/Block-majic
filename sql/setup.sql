dd5f0ad398c64a8530572bb154118dded28485bc
CarolynnFleming <cfcatpink@gmail.com>
2022-04-14 16:32:25 -0700

    sql table set up .env file setup, and user route created

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    user_id uuid DEFAULT uuid_generate_v4()
+)
