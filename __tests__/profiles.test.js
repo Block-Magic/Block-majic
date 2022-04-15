de7d6a7747e40e61ecbf6effde8d449e20005d7e
CadillacJack42 <72709991+CadillacJack42@users.noreply.github.com>
2022-04-15 15:57:40 -0700

    Jacks dev (#8)
    * sql table set up .env file setup, and user route created
    
    * test written for signing up a user
    
    * users controller .post route written to insert a user
    
    * user model creadted to insert an email and password in to the users table
    
    * .env file created. create user in usrservice writn
    
    * userservice updated with password.
    
    * sign in test written
    
    * sign in user on userservice written
    
    * find by email/get password written
    
    * sign in function test passing.
    
    * updated test to test for redirect. added homepage route to controller
    
    * wrote getAllUsers function in User model
    
    * test redirecting to homepage
    
    * added secrets to yml file
    
    * fixed yml file
    
    * test: Should delete user cookie on sign out
    
    * feat: Controller route for sign out removes cookie from user object
    
    * feat: write module to generate public and private keys
    
    * feat: profile table set up
    
    * test: Should create user profile on sign up
    
    * feat: Profile model insertProfile method
    
    * feat: added getbyid to Profile model, test passes
    
    Co-authored-by: CarolynnFleming <cfcatpink@gmail.com>
    Co-authored-by: Pete Seeling <‘peteseeling@gmail.com’>
    Co-authored-by: Pete Seeling <90798603+PeteSeeling@users.noreply.github.com>
    Co-authored-by: Philippe Ngom <philngom@gmail.com>

---
+++ __tests__/profiles.test.js
@@ -0,0 +1,32 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+const ProfileService = require('../lib/services/ProfileService');
+const Profile = require('../lib/models/Profile');
+
+describe('block-majic profile routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('Should create profile when a new user signs up', async () => {
+    const user = await UserService.createUser({
+      email: 'caddaddy@car.com',
+      password: '123456',
+    });
+
+    const profile = await Profile.getById(user.id);
+
+    expect(profile).toEqual({
+      publicKey: expect.any(String),
+      balance: '100',
+      id: expect.any(String),
+    });
+  });
+});
