e5f5f3b5ae31171750a25b8521564056013ba7b0
James Keicher <jamesdkeicher@gmail.com>
2022-04-15 14:04:49 -0700

    feat: profile table set up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS profiles CASCADE;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     email TEXT NOT NULL,
     password TEXT NOT NULL,
-    user_id uuid DEFAULT uuid_generate_v4()
-)
+    user_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY
+);
+
+CREATE TABLE profiles (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id uuid REFERENCES users(user_id) UNIQUE, 
+    public_key TEXT NOT NULL,
+    private_key TEXT NOT NULL,
+    wallet_balance BIGINT NOT NULL
+);
