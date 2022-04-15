c7da1cf338daec95d30f5444f0bc57172b3cf230
James Keicher <jamesdkeicher@gmail.com>
2022-04-15 12:22:23 -0700

    feat: Controller route for sign out removes cookie from user object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,14 +18,18 @@ describe('block-majic routes', () => {
       .post('/api/v1/users')
       .send({ email: 'dunderhead@blah.com', password: 'yourmomrules' });
 
-    expect(res.body).toEqual({ id: expect.any(String), email: 'dunderhead@blah.com' });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: 'dunderhead@blah.com',
+    });
   });
 
   it('signs in an existing user', async () => {
     const agent = request.agent(app);
 
     const user = await UserService.createUser({
-      email: 'dunderhead@blah.com', password: 'yourmomrules'
+      email: 'dunderhead@blah.com',
+      password: 'yourmomrules',
     });
 
     const { email } = user;
@@ -35,23 +39,27 @@ describe('block-majic routes', () => {
       .send({ email, password: 'yourmomrules' })
       .redirects(1);
 
-
     expect(res.req.path).toEqual('/api/v1/users/homepage');
   });
 
   it('Should delete cookie from user object', async () => {
+    const user = await UserService.createUser({
+      email: 'dunderhead@blah.com',
+      password: 'yourmomrules',
+    });
     const agent = request.agent(app);
 
-    await agent.post('/api/v1/users/sessions')
-    .send({ email, password: 'yourmomrules' })
-    .redirects(1);
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: user.email, password: 'yourmomrules' })
+      .redirects(1);
 
-    const res = await agent.delete('api/v1/users/session');
+    const res = await agent.delete('/api/v1/users');
 
     expect(res.body).toEqual({
       success: true,
-      message: 'Successfully signed out!'
-    })
-    expect(res.status).toEqual(200)
-  })
+      message: 'Successfully signed out!',
+    });
+    expect(res.status).toEqual(200);
+  });
 });
