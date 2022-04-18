c6341a1426f9d38a71718a8c8a7ee6f2c48e1895
CarolynnFleming <cfcatpink@gmail.com>
2022-04-18 14:30:40 -0700

    test passing control written for blockchain

--- __tests__/blockchain.test.js
+++ __tests__/blockchain.test.js
@@ -3,9 +3,27 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
-const ProfileService = require('../lib/services/ProfileService');
-const Profile = require('../lib/models/Profile');
+const Blockchain = require('../lib/models/Blockchain');
+// const ProfileService = require('../lib/services/ProfileService');
+// const Profile = require('../lib/models/Profile');
 
+
+// const signUpAndLogin = async () => {
+//   const agent = request.agent(app);
+  
+//   const user = await UserService.createUser({
+//     email: 'dunderhead@blah.com',
+//     password: 'yourmomrules',
+//   });
+  
+//   const { email } = user;
+  
+//   await agent
+//     .post('/api/v1/users/sessions')
+//     .send({ email, password: 'yourmomrules' });
+  
+//   return agent;
+// };
 describe('block-majic Blockchain routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -15,7 +33,18 @@ describe('block-majic Blockchain routes', () => {
     pool.end();
   });
 
-  it.skip('Should do athing', async () => {
-    // const res = await Blockchain.doathing();
+  it('should return a new block from function create new block', async () => {
+    const blockChain = new Blockchain;
+    const res = await blockChain.addNewBlock({ hash: 'ASHD7368276E', previousHash: 'KAENDF72R87Y37', nonce: 2345 });
+
+    expect(res).toEqual({
+      index: 1,
+      timestamp: expect.any(Number),
+      transactions: expect.any(Array),
+      nonce: 2345,
+      hash: 'ASHD7368276E',
+      previousHash: 'KAENDF72R87Y37',
+    });
+
   });
 });
