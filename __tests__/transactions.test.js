d3b07e5d13f27009d6285a83b19c712320eb06f0
Philippe Ngom <philngom@gmail.com>
2022-04-19 13:00:42 -0700

    wrote test #1 for transactions test. not passing yet

--- __tests__/transactions.test.js
+++ __tests__/transactions.test.js
@@ -9,19 +9,26 @@ const Profile = require('../lib/models/Profile');
 
 const signUpAndLogin = async () => {
   const agent = request.agent(app);
-    
-  const user = await UserService.createUser({
+
+  let user = await UserService.createUser({
     email: 'dunderhead@blah.com',
     password: 'yourmomrules',
   });
-    
+
+  let receiver = await UserService.createUser({
+    email: 'receiver@blah.com',
+    password: 'yourmomrules',
+  });
+
   const { email } = user;
-    
+
   await agent
     .post('/api/v1/users/sessions')
     .send({ email, password: 'yourmomrules' });
 
-  return [agent, user];
+  user = await Profile.getById(user.id);
+  receiver = await Profile.getById(receiver.id);
+  return [agent, user, receiver];
 };
 
 describe('block-majic profile routes', () => {
@@ -34,22 +41,22 @@ describe('block-majic profile routes', () => {
   });
 
   it('Should create a new transaction and add to DB', async () => {
-    const [agent, user] = await signUpAndLogin();
-
-    const res = await agent.post('/api/v1/transactions').send({ amount:'100', senderId: user.id, receiverId:'ASDFDF' });
+    const [agent, user, receiver] = await signUpAndLogin();
+    console.log('userrrrrrr', user.id);
+    const res = await agent.post('/api/v1/transactions').send({ amount:'100', sender: user.publicKey, receiver: receiver.publicKey });
 
     expect(res.body).toEqual({
-      amount:'100', senderId: user.id, receiverId:'ASDFDF', timestamp: expect.any(String)
+      amount:'100', senderId: user.publicKey, receiverId: receiver.publicKey, timestamp: expect.any(String)
     });
   });
 
-  it('Should create a new block when 10 transactions have been created', async () => {
-    const [agent, user] = await signUpAndLogin();
+  // it('Should create a new block when 10 transactions have been created', async () => {
+  //   const [agent, user] = await signUpAndLogin();
 
-    for (let i = 0; i < 10; i++) {
-      await agent.post('/api/v1/transactions').send({ amount:'1', senderId: user.id, receiverId:'ASDFDF' });
-    }
+  //   for (let i = 0; i < 10; i++) {
+  //     await agent.post('/api/v1/transactions').send({ amount:'1', senderId: user.id, receiverId:'ASDFDF' });
+  //   }
 
-  });
+  // });
 });
 
